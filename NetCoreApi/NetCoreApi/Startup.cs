ae8cb5742991709c69fba015ece4df10fc0e9eec
Stig Hausberg <stig.haugsberg@gmail.com>
2019-04-24 19:05:57 +0200

    endret noen nuget pakker

--- NetCoreApi/NetCoreApi/Startup.cs
+++ NetCoreApi/NetCoreApi/Startup.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
